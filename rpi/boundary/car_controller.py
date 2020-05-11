21694b78dda55260bf1eced408aeb525d287649b
NPseba <neteda.sebastian@gmail.com>
2020-05-11 09:04:58 +0300

    add Image Analysis Service

--- rpi/boundary/car_controller.py
+++ rpi/boundary/car_controller.py
@@ -3,19 +3,26 @@ import time
 from flask import Flask, jsonify
 
 from car.car import Car
+from service.image_analysis import ImageAnalysisService
 
 
 class CarController:
-    def __init__(self, car: Car, flask_app: Flask):
+
+    def __init__(self, car: Car, flask_app: Flask,  image_analysis_service: ImageAnalysisService):
+
         self._car = car
         self._flaskApp = flask_app
+        self._imageAnalysisService = image_analysis_service
+
         self._flaskApp.route("/status", methods=['GET'])(self.status)
 
     def status(self):
         return jsonify(status=str(self._car.status.name))
 
     def run(self):
+
         for i in range(5):
+
             self._car.move_forward()
             self._car.stop()
             self._car.take_picture('img_' + str(i) + '.png')
